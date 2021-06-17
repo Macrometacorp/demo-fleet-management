66f007bbac722b5a32155858d38ab520c000809c
raghavendra-macrometa <raghavendra.edem@macrometa.com>
2021-06-17 17:06:02 +0530

    add gdn

--- src/app/services/config.js
+++ src/app/services/config.js
@@ -1,14 +1,6 @@
 const config = {
-    // gdnURL: "https://sol1.eng.macrometa.io",
-    gdnURL: "https://tushar.eng.macrometa.io",
-    // apiKey: "hitachi-demo_macrometa.com.hitachi_demo.Sipj6ajlNheMLd6aBXGaFRyEheawz6rj8Q5QIaFkAcqH7KwQZdETeBPVKwj09Lti50387a",
-    // apiKey: "hitachi.L0UUaoxiFqfd6gEhJp306JdpFs6xBSOJB9yo3dWQgCywxcT6JENDTnlV1AYpuzGRaa4d54",
-    apiKey: "hitachidemo_macrometa.io.demoHitachi.rOoXmvQYfkHRetX4MaDVLfHyNeU83JRsV27CDILLygTs4g53YhmNSJ5n4DOrOukx965e82",
-    // listSecuritiesQueryName: "listSecurities",
-    // listTopNegativeSecuritiesQueryName: "getNegativeSecurities",
-    // listTopNeutralSecuritiesQueryName: "getNeutralSecurities",
-    // listTopPositiveSecuritiesQueryName: "getPositiveSecurities",
-    // streamAppsApiEndpoint: "https://api-gdn.paas.macrometa.io/_fabric/_system/_api/streamapps/",
+    gdnURL: "https://gdn.paas.macrometa.io",
+    apiKey: "XXXXX",
 };
 
 export default config;
